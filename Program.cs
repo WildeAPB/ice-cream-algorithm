323ab4ac7d7b44dcbdacc173ae6b22d5ff0799d9
WildeAPB <48721781+WildeAPB@users.noreply.github.com>
2019-09-27 15:58:14 +0100

    Update Program.cs
    - Added 3 classes for the 3 main processes that the system will need to do. Possiblity of adding a 4th later.
    
    - Created the Input Number of Users method.
    
    - Created ~50% of the Input Types of Ice Cream.
    
    - Attempted to call classes in Main() method, however currently an issue.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ice_cream_algorithm
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            InputNumberUsers inputNumberUsers = new InputNumberUsers();
+            IceCreamInput iceCreamInput = new IceCreamInput();
+            IceCreamDecider iceCreamDecider = new IceCreamDecider();
+
+            inputNumberUsers.inputNumberOfUsers();
+
         }
+    }
+
+
+    class InputNumberUsers
+    {
+        public static int inputNumberOfUsers()
+        {
+            Console.WriteLine("Please enter the number of users:");
+            int.TryParse(Console.ReadLine(), out int numberOfUsers);
+            return numberOfUsers;
+        }
+    }
+    
+    class IceCreamInput
+    {
+        public static string[] iceCreamSelection()
+        {
+            List<string> iceCreams = new List<string>{};
+            bool enteringIceCreams = true;
+            while(enteringIceCreams == true)
+            {
+                Console.WriteLine("Current Ice Creams:");
+                foreach (string i in iceCreams) {
+                    Console.Write("{0}\t", i.ToString()); }
+                string lastEntry = Console.ReadLine();
+                if(lastEntry == "Exit")
+                {
+                    enteringIceCreams = false;
+                }
+                else
+                {
+                    iceCreams.Add(lastEntry);
+                }
+            }
+            return iceCreams.ToArray();
+        }
+    }
+
+    class IceCreamDecider
+    {
+
     }
 }
