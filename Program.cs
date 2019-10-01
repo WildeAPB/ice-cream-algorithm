6ae45c2f3fe17711e58524c0a624c71302723b61
WildeAPB <48721781+WildeAPB@users.noreply.github.com>
2019-10-01 18:03:39 +0100

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 namespace ice_cream_algorithm
 {
 
@@ -7,20 +8,40 @@ namespace ice_cream_algorithm
     {
         static void Main(string[] args)
         {
-            int numberOfUsers = InputNumberUsers.inputNumberOfUsers();
+            string[] users = InputNumberUsers.inputNumberOfUsers();
             string[] iceCreamSelection = IceCreamInput.iceCreamSelection();
-            IceCreamDecider.IceCreamAlgorithm(numberOfUsers, iceCreamSelection);
+            IceCreamDecider.IceCreamAlgorithm(users, iceCreamSelection);
         }
     }
 
 
     class InputNumberUsers
     {
-        public static int inputNumberOfUsers()
+        public static string[] inputNumberOfUsers()
         {
-            Console.WriteLine("Please enter the number of users:");
-            int.TryParse(Console.ReadLine(), out int numberOfUsers);
-            return numberOfUsers;
+            List<string> users = new List<string>{};
+            bool enteringUsers = true;
+            while(enteringUsers == true)
+            {
+                Console.Clear();
+                Console.WriteLine("--------------------");
+                Console.Write("Current Users: ");
+                foreach (string user in users) {
+                    Console.Write(user.ToString() + ", "); }
+                Console.WriteLine();
+                Console.WriteLine("-------------------");
+                Console.WriteLine("User Data Entry:");
+                Console.WriteLine("Keywords: Exit = Close Program, Done = Finished Entering Data, Reset = Clear Current Users");
+                Console.Write("Please enter the users: ");
+                Console.WriteLine();
+                Console.WriteLine("-------------------");
+                string lastEntry = Console.ReadLine();
+                if(lastEntry == "Clear") {users.Clear();}
+                else if(lastEntry == "Exit") {Environment.Exit(0);}
+                else if(lastEntry == "Done") {enteringUsers = false;}
+                else {users.Add(lastEntry);}
+            }
+            return users.ToArray();
         }
     }
     
@@ -35,8 +56,8 @@ namespace ice_cream_algorithm
                 Console.Clear();
                 Console.WriteLine("-------------------");
                 Console.Write("Current Ice Creams: ");
-                foreach (string i in iceCreams) {
-                    Console.Write(i.ToString() + ", "); }
+                foreach (string iceCream in iceCreams) {
+                    Console.Write(iceCream.ToString() + ", "); }
                 Console.WriteLine();
                 Console.WriteLine("-------------------");
                 Console.WriteLine("Ice Cream Data Entry:");
@@ -57,12 +78,13 @@ namespace ice_cream_algorithm
 
     class IceCreamDecider
     {
-        public static void IceCreamAlgorithm(int numberOfUsers, string[] iceCreams){
-            for(int i = 0; i < numberOfUsers; i++)
+        public static void IceCreamAlgorithm(string[] users, string[] iceCreams){
+            Dictionary<string, string> userChoices = new Dictionary<string, string>();
+            foreach(var user in users)
             {
                 Console.Clear();
                 Console.WriteLine("-----------------------------------------------------------------------");
-                Console.WriteLine("User " + i + ", please choose an ice cream flavour from the list below.");
+                Console.WriteLine("User " + user + ", please choose an ice cream flavour from the list below.");
                 Console.WriteLine("Use the number keys to choose your selection.");
                 Console.WriteLine("-----------------------------------------------------------------------");
                 for(int index = 0; index < iceCreams.Length; index++)
@@ -70,6 +92,7 @@ namespace ice_cream_algorithm
                     Console.WriteLine((index + 1) + ") " + iceCreams[index]);
                 }
                 Console.WriteLine("-----------------------------------------------------------------------");
+                userIceCreamChoice = 
                 
             }
         }
